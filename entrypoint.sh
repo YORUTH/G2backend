9d131749c389219c8eac69835c94cb433363733b
Yonas Debru <101582007+Yonas567@users.noreply.github.com>
2025-11-27 22:44:13 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-set -e; echo "Making migrations..."; python manage.py makemigrations; echo "Applying migrations..."; python manage.py migrate; echo "Starting server..."; exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 4 --access-logfile - --error-logfile - --log-level info
+set -e; echo "Making migrations..."; python manage.py makemigrations; echo "Applying migrations..."; python manage.py migrate; echo "Starting server..."; exec gunicorn core.wsgi:application --bind 0.0.0.0:8080 --workers 4 --access-logfile - --error-logfile - --log-level info
